3a2e8cdddf1f097b9613666575d8274e15b02397
hangocdinhnew <dinhndrwnew@gmail.com>
2025-06-13 22:18:23 +0700

    More efficient?

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // clang-format off
 #include <iostream>
 #include <fstream>
+#include <llvm/Support/CodeGen.h>
 #include <sstream>
 #include <string>
 #include <cstdlib>
@@ -26,9 +27,14 @@ yyFlexLexer lexer;
 int yylex() { return lexer.yylex(); }
 
 int main(int argc, char **argv) {
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+  llvm::InitializeNativeTargetAsmParser();
+
   std::string inputFile;
   std::string outFile;
   bool runIt = false, emitOnlyIR = false, noCleanup = false;
+  bool compileOnly = false;
 
   for (int i = 1; i < argc; ++i) {
     std::string arg = argv[i];
@@ -40,6 +46,8 @@ int main(int argc, char **argv) {
       outFile = argv[++i];
     else if (arg == "--no-cleanup")
       noCleanup = true;
+    else if (arg == "--compile-only" || arg == "-C")
+      compileOnly = true;
     else {
       inputFile = arg;
 #ifdef _WIN32
@@ -59,11 +67,12 @@ int main(int argc, char **argv) {
   if (inputFile.empty()) {
     std::cerr << "Usage: bcc [options] <file.b>\n"
               << "Options:\n"
-              << "\t--emit-only-ir | -ir\t\tShow IR and exit (no compilation)\n"
+              << "\t--emit-only-ir | -ir\t\tEmit IR and exit (no compilation)\n"
               << "\t--run\t\t\t\tRun the compiled binary\n"
               << "\t--out | -o <filename>\t\tSet output binary name (default: "
                  "out)\n"
-              << "\t--no-cleanup\t\t\tDoesn't remove all of the IR files\n";
+              << "\t--no-cleanup\t\t\tDoesn't remove all of the IR files\n"
+              << "\t--compile-only | -C\t\tOnly compiles without linking.\n";
     return 1;
   }
 
@@ -78,7 +87,7 @@ int main(int argc, char **argv) {
 
   std::istringstream input(code);
   lexer.switch_streams(&input, nullptr);
-  yyparse(); // Fills `root`
+  yyparse();
 
   for (auto &ext : bcc::externs) {
     ext->codegen();
@@ -89,45 +98,87 @@ int main(int argc, char **argv) {
   }
 
   if (!bcc::FunctionProtos.count("main")) {
-    std::cerr << "Error: no main function defined!\n";
     return 1;
   }
 
   if (emitOnlyIR) {
-    bcc::TheModule->print(llvm::outs(), nullptr);
+    std::error_code ec;
+    std::string irFileName = outFile + ".ll";
+    llvm::raw_fd_ostream irOut(irFileName, ec, llvm::sys::fs::OF_None);
+
+    if (ec) {
+      std::cerr << "Could not open IR file for writing! Error: " << ec.message()
+                << "\n";
+      return 1;
+    }
+
+    bcc::TheModule->print(irOut, nullptr);
+    irOut.flush();
+
+    std::cout << "Successfully emitted IR: " << irFileName << "\n";
     return 0;
   }
 
-  // Compile to object file
-  std::string llFile = outFile + ".ll";
+  auto targetTriple = llvm::sys::getDefaultTargetTriple();
+  bcc::TheModule->setTargetTriple(targetTriple);
 
-  std::error_code EC;
-  llvm::raw_fd_ostream out(llFile, EC, llvm::sys::fs::OF_Text);
-  if (EC) {
-    std::cerr << "Could not write IR: " << EC.message() << "\n";
-    return 1;
-  }
-  bcc::TheModule->print(out, nullptr);
-  out.flush();
-
-  // Link to executable
-  std::string linkCmd = "clang -x ir " + llFile + " -o " + outFile;
-  int rc = system(linkCmd.c_str());
-  if (rc != 0) {
-    std::cerr << "Linking failed.\n";
+  std::string error;
+  auto target = llvm::TargetRegistry::lookupTarget(targetTriple, error);
+  if (!target) {
+    std::cerr << "Failed to lookup target! Error: " << error << "\n";
     return 1;
   }
 
-  if (!noCleanup) {
-    if (std::remove(llFile.c_str()) != 0) {
-      std::cerr << "Warning: Failed to delete intermediate file: " << llFile
-                << "\n";
+  auto cpu = "generic";
+  auto features = "";
+
+  llvm::TargetOptions opt;
+  auto RM = llvm::Reloc::PIC_;
+  auto targetMachine =
+      target->createTargetMachine(targetTriple, cpu, features, opt, RM);
+  bcc::TheModule->setDataLayout(targetMachine->createDataLayout());
+
+#ifdef _WIN32
+  std::string objFileName = outFile + ".obj";
+#else
+  std::string objFileName = outFile + ".o";
+#endif
+
+  std::error_code ec;
+  llvm::raw_fd_ostream dest(objFileName, ec, llvm::sys::fs::OF_None);
+
+  if (ec) {
+    std::cerr << "Could not open file! Error: " << ec.message() << "\n";
+    return 1;
+  }
+
+  llvm::legacy::PassManager pass;
+  if (targetMachine->addPassesToEmitFile(pass, dest, nullptr,
+                                         llvm::CodeGenFileType::ObjectFile)) {
+    std::cerr << "TargetMachine can't emit a file of this type!\n";
+  }
+
+  pass.run(*bcc::TheModule);
+  dest.flush();
+
+  if (!compileOnly) {
+    std::string linkCmd = "clang " + objFileName + " -o " + outFile;
+
+    int linkResult = system(linkCmd.c_str());
+    if (linkResult != 0) {
+      std::cerr << "Linking failed!\n";
+      return 1;
     }
   }
 
   if (runIt) {
+#ifdef _WIN32
+    std::string execCmd = ".\" + outFile;
+#else
     std::string execCmd = "./" + outFile;
-    return system(execCmd.c_str());
+#endif
+
+        return system(execCmd.c_str());
   }
 
   return 0;
